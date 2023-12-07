e323a1f61b0577875d3106bb534cec899c9d50d9
fy <fy77645@126.com>
2023-12-07 16:48:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { PORT } from './config/config'
+
+
+
+const app = express()
+
+
+
+app.get('/',(req,res)=>{
+  res.end('node server.')
+})
+
+
+app.listen(PORT,()=>{
+  console.log('node server is running.')
+})
